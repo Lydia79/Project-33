c42b9b0e2ab14a15c462dffa6ea25be4ee28ca5b
Lydia79 <77635930+Lydia79@users.noreply.github.com>
2021-07-16 17:30:51 +0100

    Add files via upload

---
+++ Boy.js
@@ -0,0 +1,9 @@
+class Boy{
+constructor(x,y,width,height){
+    var options = {
+        'restitution':0.8,
+        'friction':0.1,
+        'density':1.0
+    }
+}
+}
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var bg;
+var boy;
+
+function preload(){
+  bg = loadImage("snow1.jpg");
+  boy = loadImage("snowfall.jpg");
+}
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(bg);  
+  drawSprites();
+}
+
+
